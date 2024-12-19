25d76ab437df96377332fa596659590c25fef3df
Intan <mohdfaddilintan@gmail.com>
2024-12-19 19:57:13 +0800

    correcting text below the box

--- lib/widgets/ItemsWidget.dart
+++ lib/widgets/ItemsWidget.dart
@@ -3,58 +3,50 @@ import 'package:flutter/material.dart';
 class ItemsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.count(
-      childAspectRatio: 1.0,
+      childAspectRatio: 0.7, // Keep the boxes square
       physics: NeverScrollableScrollPhysics(),
       crossAxisCount: 4,
       shrinkWrap: true,
       children: [
-        for(int i = 1; i<5; i++)
-          Container(
-            height: 200,
-            width: 55,
-            padding: EdgeInsets.only(left: 15, right: 10, top: 10),
-            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-            decoration: BoxDecoration(
-              color: Color(0xFFF1D789),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              InkWell(
-                onTap: () {
-                  //Navigator.pushNamed(context, itemPage);
-                },
-                child: Container(
-                  margin: EdgeInsets.all(5),
-                  child: SizedBox(
-                    child: Image.asset(
-                      alignment: Alignment.center,
-                      "assets/iconhome$i.png",
-                      fit: BoxFit.cover,
-                    ),
+        for (int i = 1; i < 5; i++)
+          Column(
+            children: [
+              Container(
+                height: 85, // Fixed height
+                width: 85,  // Fixed width
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Color(0xFFF1D789),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    //Navigator.pushNamed(context, itemPage);
+                  },
+                  child: Image.asset(
+                    "assets/iconhome$i.png",
+                    alignment: Alignment.center,
+                    fit: BoxFit.contain,
                   ),
                 ),
               ),
-                Padding(
-                  padding: EdgeInsets.only(top: 30),
-                  child: Text(
-                    getTextForIndex(i),
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
+              SizedBox(height: 8),
+              Text(
+                getTextForIndex(i),
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
-              ],
-            ),
+                textAlign: TextAlign.center,
+              ),
+            ],
           ),
       ],
     );
   }
 }
+
 String getTextForIndex(int index) {
   switch (index) {
     case 1:

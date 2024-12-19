4d7cbe23a55a6aa8c8b0e5f57efea3b743b2f845
Intan <mohdfaddilintan@gmail.com>
2024-12-19 13:17:42 +0800

    adding text

--- lib/widgets/ItemsWidget.dart
+++ lib/widgets/ItemsWidget.dart
@@ -3,16 +3,15 @@ import 'package:flutter/material.dart';
 class ItemsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.count(
-      //childAspectRatio: 0.72,
+      childAspectRatio: 1.0,
       physics: NeverScrollableScrollPhysics(),
       crossAxisCount: 4,
       shrinkWrap: true,
       children: [
         for(int i = 1; i<5; i++)
           Container(
-            //15:46
-            height: 100,
-            width: 100,
+            height: 200,
+            width: 55,
             padding: EdgeInsets.only(left: 15, right: 10, top: 10),
             margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
             decoration: BoxDecoration(
@@ -29,21 +28,44 @@ class ItemsWidget extends StatelessWidget {
                 child: Container(
                   margin: EdgeInsets.all(5),
                   child: SizedBox(
-                    width: 55,
-                    height: 55,
                     child: Image.asset(
                       alignment: Alignment.center,
-                      //width: 100,
-                     //height: 100,
                       "assets/iconhome$i.png",
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
               ),
-            ],),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child: Text(
+                    getTextForIndex(i),
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
           ),
       ],
     );
+  }
+}
+String getTextForIndex(int index) {
+  switch (index) {
+    case 1:
+      return "Home";
+    case 2:
+      return "Documents";
+    case 3:
+      return "Messages";
+    case 4:
+      return "Gifts";
+    default:
+      return "";
   }
 }
